1c1c107880aac8f8aea0b4f96d00ebb31be8199a
ZHD <13417014227@163.com>
2025-04-29 20:28:32 +0800

    第一个分支，同时也成功在vscodebuild和运行

---
+++ build/CMakeFiles/test1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/A_codes/VS_code/cmake_test/src/main.cpp" "CMakeFiles/test1.dir/src/main.cpp.obj" "gcc" "CMakeFiles/test1.dir/src/main.cpp.obj.d"
+  "E:/A_codes/VS_code/cmake_test/src/view/test1.cpp" "CMakeFiles/test1.dir/src/view/test1.cpp.obj" "gcc" "CMakeFiles/test1.dir/src/view/test1.cpp.obj.d"
+  "E:/A_codes/VS_code/cmake_test/build/test1_autogen/mocs_compilation.cpp" "CMakeFiles/test1.dir/test1_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/test1.dir/test1_autogen/mocs_compilation.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test1.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test1.dir/src/main.cpp.obj"
+  "CMakeFiles/test1.dir/src/main.cpp.obj.d"
+  "CMakeFiles/test1.dir/src/view/test1.cpp.obj"
+  "CMakeFiles/test1.dir/src/view/test1.cpp.obj.d"
+  "CMakeFiles/test1.dir/test1_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/test1.dir/test1_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/test1_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/test1_autogen.dir/ParseCache.txt"
+  "test1_autogen"
+  "libtest1.dll.a"
+  "test1.exe"
+  "test1.exe.manifest"
+  "test1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
